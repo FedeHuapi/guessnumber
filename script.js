632859016d955e2e6db1cc11ae4c1a924958eee8
Federico <federicocurto00@gmail.com>
2025-03-25 19:34:26 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+
+console.log(numeroSecreto);
+
+const input = document.getElementById("input")
+const guessBtn = document.getElementById("guess")
+const mensaje = document.getElementById("ptext")
+const intentosText = document.getElementById("intentos")
+const resetBtn = document.getElementById("reset")
+
+let intentos = 0;
+let juegoTerminado = false;
+
+guessBtn.addEventListener("click", function () {
+    let adivinanza = parseInt(input.value);
+    
+    
+    if(isNaN(adivinanza) || adivinanza < 1 || adivinanza > 10) {
+        mensaje.textContent = "Por favor, ingresá un número válido entre 1 y 10"
+        return;
+    }
+    
+    intentos++;
+    intentosText.textContent = "Intentos: " + intentos;
+
+    if (adivinanza ===  numeroSecreto) {
+        mensaje.textContent = "¡Correcto! Adivinaste el número en " + intentos + " intentos"
+        juegoTerminado = true;
+        guessBtn.disabled = true;
+
+    } else if (adivinanza > numeroSecreto) {
+        mensaje.textContent = "Demasiado alto, intenta de nuevo"
+    } else if (parseFloat(input.value) !== adivinanza) {
+        mensaje.textContent = "Por favor, ingresá un número entero"
+    } else {
+        mensaje.textContent = "Demasiado bajo, intenta de nuevo"
+    }
+    
+
+
+    console.log(adivinanza);
+    
+});
+
+resetBtn.addEventListener("click", function(){
+    juegoTerminado = false;
+    guessBtn.disabled = false;
+
+    numeroSecreto = Math.floor(Math.random() * 10) + 1
+    console.log(numeroSecreto);
+
+    intentos = 0;
+    intentosText.textContent = "Intentos: " + intentos;
+
+    input.value = "";
+
+    mensaje.textContent = "Adivina un numero del 1 al 10"
+    
+})
+
+
